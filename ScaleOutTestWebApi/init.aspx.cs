7b60e7acfc432460c381a7ca1cb1a30e82bdd892
Eric Wellnitz <ewellnitz@anthology.com>
2024-08-20 12:09:45 +0530

    initial add

---
+++ ScaleOutTestWebApi/init.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ScaleOutTestWebApi
+{
+    public partial class init : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Debug.WriteLine("init::Page_Load - Start");
+            System.Threading.Thread.SpinWait(10000);
+            Debug.WriteLine("init::Page_Load - End");
+        }
+    }
+}
